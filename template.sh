4d1b63daaa80c94ffe77ed21ced450b3613466d9
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-03-24 17:28:19 +0530

    Folder sstructure added using sh command

---
+++ template.sh
@@ -0,0 +1,19 @@
+# Creating directories
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch src/setup.py
+touch src/app.py
+touch src/trials.ipynb
+touch requirements.txt
+
+echo "Directories and files have been created successfully."
+
+
+
+
